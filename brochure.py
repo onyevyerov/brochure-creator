cbe932b0f867804782e32ca77706eb155726ba66
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-03-25 14:04:29 +0100

    Write main logic of service

---
+++ brochure.py
@@ -0,0 +1,122 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+from openai import OpenAI
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+openai = OpenAI(api_key=api_key)
+
+MODEL = 'gpt-5-nano'
+
+LINK_SYSTEM_PROMPT = """
+You are provided with a list of links found on a webpage.
+You are able to decide which of the links would be most relevant to include in a brochure about the company,
+such as links to an About page, or a Company page, or Careers/Jobs pages.
+You should respond in JSON as in this example:
+
+{
+    "links": [
+        {"type": "about page", "url": "https://full.url/goes/here/about"},
+        {"type": "careers page", "url": "https://another.full.url/careers"}
+    ]
+}
+"""
+
+BROCHURE_SYSTEM_PROMPT = """
+You are an assistant that analyzes the contents of several relevant pages from a company website
+and creates a short brochure about the company for prospective customers, investors and recruits.
+Respond in markdown without code blocks.
+Include details of company culture, customers and careers/jobs if you have the information.
+"""
+
+def fetch_website_contents(url):
+    """
+    Return the title and contents of the website at the given url;
+    truncate to 2,000 characters as a sensible limit
+    """
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    title = soup.title.string if soup.title else "No title found"
+    if soup.body:
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        text = soup.body.get_text(separator="\n", strip=True)
+    else:
+        text = ""
+    return (title + "\n\n" + text)[:2_000]
+
+def fetch_website_links(url):
+    """
+    Return the links on the webiste at the given url
+    """
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = [link.get("href") for link in soup.find_all("a")]
+    return [link for link in links if link]
+
+def get_links_user_prompt(url):
+    user_prompt = f"""
+Here is the list of links on the website {url} -
+Please decide which of these are relevant web links for a brochure about the company, 
+respond with the full https URL in JSON format.
+Do not include Terms of Service, Privacy, email links.
+
+Links (some might be relative links):
+
+"""
+    links = fetch_website_links(url)
+    user_prompt += "\n".join(links)
+    return user_prompt
+
+def select_relevant_links(url):
+    print(f"Selecting relevant links for {url} by calling {MODEL}")
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": LINK_SYSTEM_PROMPT},
+            {"role": "user", "content": get_links_user_prompt(url)}
+        ],
+        response_format={"type": "json_object"}
+    )
+    result = response.choices[0].message.content
+    links = json.loads(result)
+    print(f"Found {len(links['links'])} relevant links")
+    return links
+
+def fetch_page_and_all_relevant_links(url):
+    contents = fetch_website_contents(url)
+    relevant_links = select_relevant_links(url)
+    result = f"## Landing Page:\n\n{contents}\n## Relevant Links:\n"
+    for link in relevant_links['links']:
+        result += f"\n\n### Link: {link['type']}\n"
+        result += fetch_website_contents(link["url"])
+    return result
+
+def get_brochure_user_prompt(company_name, url):
+    user_prompt = f"""
+You are looking at a company called: {company_name}
+Here are the contents of its landing page and other relevant pages;
+use this information to build a short brochure of the company in markdown without code blocks.\n\n
+"""
+    user_prompt += fetch_page_and_all_relevant_links(url)
+    user_prompt = user_prompt[:5_000]
+    return user_prompt
+
+def create_brochure(company_name, url):
+    response = openai.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "system", "content": BROCHURE_SYSTEM_PROMPT},
+            {"role": "user", "content": get_brochure_user_prompt(company_name, url)}
+        ],
+    )
+    result = response.choices[0].message.content
+    return result
